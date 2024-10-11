7c4fff321abd248e5fb34ff92f8657b5c1281b0f
hkpqw <hkpqw.o@mail.ru>
2024-10-11 11:00:44 +0300

    new column

--- app.py
+++ app.py
@@ -3,49 +3,77 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
+from models import db, create_task_model, Executor 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/control_saz'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/control_saz' 
+db.init_app(app)
 Bootstrap(app)
 migrate = Migrate(app, db)
 
-from models import create_task_model  # Импортируем функцию
-Task = create_task_model(db)  # Импорт Task после инициализации db
+Task = create_task_model(db)
 
 @app.route('/')
 def index():
-    tasks = Task.query.order_by(Task.is_valid.desc(), Task.deadline, Task.date_created.desc()).all()
-    return render_template('index.html', tasks=tasks)
+    executor_filter = request.args.get('executor')
+    date_filter = request.args.get('date')
+
+    tasks = Task.query
+    if executor_filter:
+        tasks = tasks.filter_by(executor_id=Executor.query.filter_by(name=executor_filter).first().id) 
+    if date_filter:
+        date_filter = datetime.strptime(date_filter, '%Y-%m-%d').date()
+        tasks = tasks.filter(db.cast(Task.date_created, db.Date) == date_filter) 
+
+    tasks = tasks.order_by(Task.is_valid.desc(), Task.deadline, Task.date_created.desc()).all()
+    executors = Executor.query.all()
+    return render_template('index.html', tasks=tasks, executors=executors)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    executors = Executor.query.all()
     if request.method == 'POST':
         if request.form['password'] == '1234':
-            executor = request.form['executor']
+            executor_name = request.form['executor']
+            executor = Executor.query.filter_by(name=executor_name).first()
+            if not executor:
+                new_executor = Executor(name=executor_name)
+                db.session.add(new_executor)
+                db.session.commit()
+                executor = new_executor 
+
             date_created = datetime.strptime(request.form['date_created'], '%Y-%m-%d').date()
             deadline = datetime.strptime(request.form['deadline'], '%Y-%m-%d').date()
             description = request.form['description']
             is_valid = request.form.get('is_valid') == 'on'
 
-            new_task = Task(executor=executor, date_created=date_created, deadline=deadline, description=description, is_valid=is_valid)
+            new_task = Task(executor_id=executor.id, date_created=date_created, deadline=deadline, description=description, is_valid=is_valid)
             db.session.add(new_task)
             db.session.commit()
             flash('Задача успешно добавлена!', 'success')
             return redirect(url_for('index'))
         else:
             flash('Неверный пароль!', 'danger')
-    return render_template('add.html')
+    return render_template('add.html', executors=executors, datetime=datetime)
 
 
 @app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
 def edit(task_id):
     task = Task.query.get_or_404(task_id)
+    executors = Executor.query.all()
     if request.method == 'POST':
         if request.form['password'] == '1234':
-            # Обработка редактирования задачи
-            task.executor = request.form['executor']
+            executor_name = request.form['executor']
+            executor = Executor.query.filter_by(name=executor_name).first()
+            if not executor:
+                new_executor = Executor(name=executor_name)
+                db.session.add(new_executor)
+                db.session.commit()
+                executor = new_executor
+
+            task.executor_id = executor.id
             task.date_created = datetime.strptime(request.form['date_created'], '%Y-%m-%d').date()
             task.deadline = datetime.strptime(request.form['deadline'], '%Y-%m-%d').date()
             task.description = request.form['description']
@@ -55,7 +83,8 @@ def edit(task_id):
             return redirect(url_for('index'))
         else:
             flash('Неверный пароль!', 'danger')
-    return render_template('edit.html', task=task)
+    return render_template('edit.html', task=task, executors=executors, datetime=datetime)
+
 
 @app.route('/delete/<int:task_id>', methods=['POST']) 
 def delete(task_id):
@@ -68,5 +97,39 @@ def delete(task_id):
         flash('Неверный пароль!', 'danger')
     return redirect(url_for('index'))
 
+@app.route('/executors')
+def executors():
+    executors = Executor.query.all()
+    return render_template('executors.html', executors=executors)
+
+@app.route('/add_executor', methods=['GET', 'POST'])
+def add_executor():
+    if request.method == 'POST':
+        if request.form['password'] == '1234':
+            name = request.form['name']
+            new_executor = Executor(name=name)
+            db.session.add(new_executor)
+            db.session.commit()
+            flash('Исполнитель успешно добавлен!', 'success')
+            return redirect(url_for('executors'))
+        else:
+            flash('Неверный пароль!', 'danger')
+    return render_template('add_executor.html')
+
+
+@app.route('/delete_executor/<int:executor_id>', methods=['POST'])
+def delete_executor(executor_id):
+    executor = Executor.query.get_or_404(executor_id)
+    if request.form['password'] == '1234':
+        db.session.delete(executor)
+        db.session.commit()
+        flash('Исполнитель успешно удален!', 'success')
+    else:
+        flash('Неверный пароль!', 'danger')
+    return redirect(url_for('executors'))
+
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
     app.run(host='0.0.0.0', debug=True) 
--- models.py
+++ models.py
@@ -1,9 +1,21 @@
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  # Инициализация db вне функции
+
+class Executor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
 
 def create_task_model(db):
     class Task(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-        executor = db.Column(db.String(100), nullable=False)
+        executor_id = db.Column(db.Integer, db.ForeignKey('executor.id'), nullable=False)
+        executor = db.relationship('Executor', backref=db.backref('tasks', lazy=True)) 
         date_created = db.Column(db.Date, nullable=False)
         deadline = db.Column(db.Date, nullable=False)
         description = db.Column(db.Text, nullable=False)
@@ -13,5 +25,3 @@ def create_task_model(db):
             return self.deadline < datetime.today().date()
 
     return Task
-
-# Теперь Task будет создан только при вызове create_task_model(db)
