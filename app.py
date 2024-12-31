02e51e178d2e344462550aaad1d9735a1f46b9ad
Максим Береснев <beresnev_maksimka@mail.ru>
2024-12-31 09:37:08 +0300

    Fix debug

--- app.py
+++ app.py
@@ -1233,4 +1233,4 @@ app.register_blueprint(reports_bp, url_prefix='/') # Регистрируем Bl
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0')
