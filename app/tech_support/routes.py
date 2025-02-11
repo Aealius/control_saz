505c1afd7c90ebc411baac6cd6c0c75a800f4689
ASUP_YULIYA <93128899+Aealius@users.noreply.github.com>
2025-02-11 14:39:27 +0300

    add table and route for it

--- app/tech_support/routes.py
+++ app/tech_support/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import flash, render_template, request
+from flask import current_app, flash, render_template, request
 from flask_login import current_user, login_required
 from app.models import TechMessage
 from app.tech_support import bp
@@ -9,13 +9,11 @@ from app import db
 @login_required
 def tech_support():
     if request.method == 'POST':
-        tech_message_id = str(len(db.session.query(TechMessage).all()) + 1)
         date_created = datetime.now()
         description = request.form['description']
         comp_number = request.form['compNumber']
                 
         new_tech_message = TechMessage(
-                id = tech_message_id,
                 date_created=date_created,
                 description=description,
                 user_id=current_user.id,
@@ -26,3 +24,12 @@ def tech_support():
         flash('Сообщение успешно отправлено!', 'success')
         return '', 200
     return render_template('tech_support/tech_support.html')
+
+@bp.route('/support_table', methods=['GET'])
+@login_required
+def tech_requests():
+    
+    tech_messages = db.session.query(TechMessage).all()
+    
+    return render_template('tech_support/support_table.html', tech_messages = tech_messages,
+                                                              per_page = current_app.config['PER_PAGE'])
