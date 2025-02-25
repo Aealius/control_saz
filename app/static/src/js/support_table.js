9d35ebbe7f7abf2c5497d798d6a417c140a07c63
ASUP_YULIYA <93128899+Aealius@users.noreply.github.com>
2025-02-25 10:59:43 +0300

    some kind of page refresh

--- app/static/src/js/support_table.js
+++ app/static/src/js/support_table.js
@@ -51,6 +51,11 @@ xMarkIcon.addEventListener('click', (e) => {
     }
 });
 
+document.getElementById('search-form').addEventListener('submit', (e) => {
+    if (searchInput.value == '')
+        searchInput.disabled = true;
+});
+
 
 // Здесь document.documentElement.innerHTML меняется напрямую, вследствие чего слетает js код, и 
 // некоторые вещи могут перестать работать. Приходится заново навешивать все ивенты на html элементы.
@@ -65,6 +70,63 @@ function updatePage() {
         })
         .then(html => {
             document.documentElement.innerHTML = html;
+
+            const searchInput = document.getElementById('search-input');
+            const xMarkIcon = document.querySelector('.search-cross');
+
+            async function setStatusInWork(issue_id) {
+                await fetch(base_url + '/tech/issue_in_work/' + issue_id, {
+                    method: "POST"
+                }).then(response => console.log(response)).then(() => {
+                    updatePage();
+                })
+            }
+            
+            async function setStatusCompleted(issue_id){
+                await fetch(base_url + '/tech/issue_completed/' + issue_id, {
+                    method: "POST"
+                }).then(response => console.log(response)).then(() => {
+                        updatePage();
+                })
+            }
+            
+            async function deleteIssue(issue_id) {
+                if (confirm('Вы уверены, что хотите удалить эту задачу?')) {
+                    await fetch(base_url + '/tech/delete/' + issue_id, {
+                        method: "DELETE"
+                    }).then(response => console.log(response)).then(() => {
+                        updatePage();
+                    })
+                }
+            }
+
+            function toggleClearButton() {
+                xMarkIcon.style.display = searchInput.value ? 'block' : 'none';
+            }
+            
+            //начальное состояние
+            toggleClearButton();
+            
+            searchInput.addEventListener('input', toggleClearButton);
+            
+            xMarkIcon.addEventListener('click', (e) => {
+                let currentURL = new URL(window.location.origin + window.location.pathname + window.location.search);
+                if (currentURL.searchParams.get("search")){
+                    currentURL.searchParams.delete("search");
+                    window.location.href = currentURL;
+                }
+                else{
+                    searchInput.value = '';
+                    toggleClearButton();
+                    searchInput.focus();
+                }
+            });
+
+            document.getElementById('search-form').addEventListener('submit', (e) => {
+                if (searchInput.value == '')
+                    searchInput.disabled = true;
+            });
+            
 
             //senderSelect = document.getElementById("select-task-sender");
         })
