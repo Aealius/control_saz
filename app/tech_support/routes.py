0a3a8e54be24301368c1182754e2ae91e77952b5
ASUP_YULIYA <93128899+Aealius@users.noreply.github.com>
2025-02-28 14:31:58 +0300

    add /archived, rework endpoints

--- app/tech_support/routes.py
+++ app/tech_support/routes.py
@@ -6,7 +6,6 @@ from app.models import TechMessage
 from app.tech_support import bp
 from app import db
 from app.tech_support.utils import filter_TechMessage_data
-import os
 
 FILTER_PARAM_KEYS = ['creator',
                     'month',
@@ -37,31 +36,25 @@ def tech_support():
         return '', 200
     return render_template('tech_support/add_issue.html')
 
+
 @bp.route('/issue_table', methods=['GET'])
 @login_required
 def tech_requests():
-    filter_params_dict = {f : request.args.get(f) for f in FILTER_PARAM_KEYS if request.args.get(f)}
-    page = request.args.get('p', 1, int)
-    
-    if current_user.department == '205 ОАСУП':
-        tech_messages = db.session.query(TechMessage)
-    else:
-        tech_messages = db.session.query(TechMessage).filter(TechMessage.user_id == current_user.id)
-    
-    tech_messages, messages_count = filter_TechMessage_data(tech_messages, page, **filter_params_dict)
-    
-    return render_template('tech_support/issue_table.html', tech_messages = tech_messages,
-                                                              per_page = current_app.config['PER_PAGE'],
-                                                              filter_params_dict = filter_params_dict,
-                                                              status = Status,
-                                                              page = page,
-                                                              messages_count = messages_count)
+    return send_filtered_data(is_archived=False)
 
+@bp.route('/archived', methods=['GET'])
+@login_required
+def archived_issues():
+    return send_filtered_data(is_archived=True)
+
+
+#далее предполагается, что пользователю придет датасет уже с только НЕ удаленными и НЕ архивированными
 
 @bp.route('/issue_completed/<int:issue_id>', methods = ['POST'])
 @login_required
 def issue_completed(issue_id):
     if current_user.department == '205 ОАСУП':
+
         issue = db.session.query(TechMessage).get(issue_id)
         issue.status_id = Status.completed.value
         issue.completion_confirmed_at = datetime.now()
@@ -81,7 +74,35 @@ def issue_in_work(issue_id):
 @login_required
 def delete_issue(issue_id):
     issue_to_delete = db.session.get(TechMessage, issue_id)
-    db.session.delete(issue_to_delete)
+    issue_to_delete.is_deleted = True
     db.session.commit()
     flash('Заявка удалена!', 'success')
     return '', 200
+
+
+#мейби поменять потом темплейт
+def send_filtered_data(is_archived):
+    filter_params_dict = {f : request.args.get(f) for f in FILTER_PARAM_KEYS if request.args.get(f)}
+    page = request.args.get('p', 1, int)
+    
+    tech_messages_query = db.session.query(TechMessage)
+    if not is_archived:
+        if current_user.department == '205 ОАСУП':
+            tech_messages = tech_messages_query.filter(TechMessage.is_deleted == False, TechMessage.is_archived ==False)
+        else:
+            tech_messages = tech_messages_query.filter(TechMessage.user_id == current_user.id, TechMessage.is_deleted == False, TechMessage.is_archived ==False)
+    else:
+        if current_user.department == '205 ОАСУП':
+            tech_messages = tech_messages_query.filter(TechMessage.is_deleted == False, TechMessage.is_archived == True)
+        else:
+            tech_messages = tech_messages_query.filter(TechMessage.user_id == current_user.id, TechMessage.is_deleted == False, TechMessage.is_archived == True)
+    
+    tech_messages, messages_count = filter_TechMessage_data(tech_messages, page, **filter_params_dict)
+    
+    return render_template('tech_support/issue_table.html', tech_messages = tech_messages,
+                                                              per_page = current_app.config['PER_PAGE'],
+                                                              filter_params_dict = filter_params_dict,
+                                                              status = Status,
+                                                              page = page,
+                                                              messages_count = messages_count,
+                                                              archive = is_archived)
