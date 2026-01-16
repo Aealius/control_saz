582a0d4d089a591f9472b40307b04804f64128f9
ASUP_YULIYA <93128899+Aealius@users.noreply.github.com>
2026-01-16 10:49:12 +0300

    update config params

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/control_saz'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/control_saz'
     JSON_AS_ASCII = False
     CAN_GET_RESENDED_TASKS_ARR = ['27']
     BUH_LOGIN = '234'
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
     LOGS_FOLDER ='logs'
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+    MAX_CONTENT_LENGTH = 25 * 1024 * 1024  # 25MB
     PER_PAGE = 20
